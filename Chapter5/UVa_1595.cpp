42710ffa9ec45162e2f63cc7a53cdf16b2032a93
WHU-ZX <1598244350@qq.com>
2020-03-20 09:26:52 +0800

    UVa 1595 通过OJ

--- Chapter5/UVa_1595.cpp
+++ Chapter5/UVa_1595.cpp
@@ -1,15 +1,22 @@
 #include <cstdio>
 #include <iostream>
+#include <map>
+#include <vector>
+#include<algorithm>
 using namespace std;
 static int gameNum;
 static int pointNum;
-
-
 struct Point
 {
 	int x;
 	int y;
 };
+static map<int, vector<Point> > m;
+
+bool cmp(Point p1, Point p2)
+{
+	return p1.x < p2.x;
+}
 
 int UVa_1595()
 {
@@ -20,13 +27,60 @@ int UVa_1595()
 	cin >> gameNum;
 	while (gameNum--)
 	{
+		m.clear();
 		cin >> pointNum;
-
-
-
-
+		Point p;
+		while (pointNum--)
+		{
+			cin >> p.x >> p.y;
+			p.x *= 2;//保证p.x是偶数
+			if (m.find(p.y) != m.end())//存在此vector
+			{
+				m[p.y].push_back(p);
+			}
+			else//不存在此vector
+			{
+				vector<Point> tP;
+				tP.push_back(p);
+				m[p.y] = tP;
+			}
+		}
+		map<int, vector<Point> >::iterator iter;
+		bool hasLine = true;
+		int cX = 0;
+		bool initial = true;
+		for (iter = m.begin(); iter != m.end(); iter++)
+		{
+			vector<Point> v = iter->second;
+			//检验v中是否是对称的，若不是，令hasLine=false并break
+			sort(v.begin(), v.end(), cmp);
+			bool naturalFinish = true;
+			for (int i = 0,j = v.size() - 1; i <= j; i++, j--)
+			{
+				if (initial)
+				{
+					cX = (v[i].x + v[j].x) / 2;
+					initial = false;
+				}
+				else
+				{
+					if ((v[i].x + v[j].x) / 2 != cX)
+					{
+						naturalFinish = false;
+						break;
+					}
+				}
+			}
+			if (!naturalFinish)//break退出，表示已经检测到没有对称轴
+			{
+				hasLine = false;
+				break;
+			}
+		}
+		if (hasLine)
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
 	}
-
-
 	return 0;
 }

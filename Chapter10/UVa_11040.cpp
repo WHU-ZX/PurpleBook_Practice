a0bed909742ffe81fa155e27fb4440965d149a4f
WHU-ZX <1598244350@qq.com>
2020-06-19 15:16:17 +0800

    初始化第十章

---
+++ Chapter10/UVa_11040.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+#pragma warning(disable:4996)
+
+
+
+
+
+int UVa_11040()
+{
+	const char* input = "inputs/11040.txt";
+	//const char* output = "outputs/11040.txt";
+	freopen(input, "r", stdin);
+
+
+
+	return 0;
+}

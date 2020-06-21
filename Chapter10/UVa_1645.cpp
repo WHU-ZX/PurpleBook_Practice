7c01baed959ff9d7b0643f3b513a976c59d520b3
WHU-ZX <1598244350@qq.com>
2020-06-21 22:43:09 +0800

    增加UVa1645

--- Chapter10/UVa_1645.cpp
+++ Chapter10/UVa_1645.cpp
@@ -6,25 +6,87 @@
 #pragma warning(disable:4996)
 using namespace std;
 
-const static int mod = 1000000007;
+const static int mod = 1000000007, maxn = 1001;
+static int dp[maxn];
 
-/*
-	lastLevelNum:上一层一共有多少个
-	restNum:到这一层还剩下多少个
-*/
-static int get(int lastLevelNum, int restNum)
+static void calculate_table()
 {
-
-
-	return 0;
+	memset(dp, 0, sizeof dp);
+	dp[1] = 1;
+	for (int i = 2; i < maxn; ++i)
+	{
+		for (int j = 1; j <= i - 1; ++j)
+		{
+			if ((i - 1) % j == 0)
+			{
+				dp[i] += dp[(i - 1) / j];
+				dp[i] %= mod;
+			}
+		}
+	}
 }
 
 int UVa_1645()
 {
 	const char* input = "inputs/1645.txt";
 	freopen(input, "r", stdin);
-
-
-
+	int cas = 0;
+	int n;
+	calculate_table();
+	while (cin >> n)
+	{
+		printf("Case %d: %d\n", ++cas, dp[n]);
+	}
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 低速版..
+///*
+//	lastLevelNum:上一层一共有多少个
+//	restNum:到这一层还剩下多少个
+//	输入保证restNum是0或者是lastLevelNum的整数倍
+//*/
+//static int get(int lastLevelNum, int restNum)
+//{
+//	if (restNum == 0) return 1;
+//	//restNum是lastLevelNum的整数倍
+//	int ans = 0;
+//	int maxNum = restNum / lastLevelNum;
+//	for (int newLevelNum = 1; newLevelNum <= maxNum; ++newLevelNum)
+//	{
+//		int newRestNum = maxNum - newLevelNum;
+//		if (newRestNum % newLevelNum == 0) // 表示这么做了之后，后面还能做下去，newRestNum为0时也可以
+//		{
+//			ans += get(newLevelNum * lastLevelNum, newRestNum * lastLevelNum);
+//			ans %= mod;
+//		}
+//	}
+//	return ans;
+//}
+//
+//int UVa_1645()
+//{
+//	const char* input = "inputs/1645.txt";
+//	freopen(input, "r", stdin);
+//	int cas = 0;
+//	int n;
+//	while (cin >> n)
+//	{
+//		printf("Case %d: %d\n", ++cas, get(1, n - 1));
+//	}
+//	return 0;
+//}
--- Chapter10/header.h
+++ Chapter10/header.h
@@ -6,3 +6,4 @@ int UVa_1644();
 int UVa_1213();
 int UVa_1210();
 int UVa_10539();
+int UVa_1645();
